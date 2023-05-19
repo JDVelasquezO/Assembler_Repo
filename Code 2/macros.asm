5933f9f99739a2a27791f510b76c40958d3c4ba1
Daniel <jdvela158@gmail.com>
2023-05-19 17:01:52 -0600

    Code 3

--- Code 2/macros.asm
+++ Code 2/macros.asm
@@ -1,4 +1,4 @@
-imprimirColor macro cadena, color
+imprimir macro cadena, color
     mov ax, @data
     mov ds, ax
 
@@ -10,142 +10,358 @@ imprimirColor macro cadena, color
     int 21h
 endm
 
-imprimirMatriz macro 
+printRegister macro register
+	push ax
+	push dx
 
-    imprimirColor espacio, 0d
-    imprimirColor espacio, 0d
-    ; Imprime Cabeceras de Columnas
+	mov dl,register
+	add dl,48 		; Se le suma 48 por el codigo ascii
+	mov ah,02h
+	int 21h
+
+	pop dx
+	pop ax
+endm
+
+ImprimirEspacio macro registro
+	push ax
+	push dx
+
+	mov registro, 13
+	CrearEspacio al
+	mov registro, 10
+	CrearEspacio al
+
+	pop dx
+	pop ax
+endm
+
+imprimirMatriz macro
+    local ciclo, ciclo2, ciclo3, ciclo4, reinicio, reinicio2, regreso1, quitChars, blackToken, moveCellImpair, regresoImpair1, validateImpair, incrementForImpairBlack, incrementForImpair, validateImpair, pairFile, regresoPair1, validatePair, moveCell, fin, fillRow, fillRow2
+
+    imprimir espacio, 0d    ; color negro
+    imprimir espacio, 0d
     xor si, si
+    ; Ciclo para imprimir las cabeceras de columnas de la matriz
     ciclo:
+        ; Se mueve hacia una variable que simula un caracter, porque el macro imprimir
+        ; realiza la impresion hasta que encuentra el simbolo de final de cadena
         mov bl, cabecerasC[si]
         mov individual[0], bl
-        imprimirColor individual, 15d
-        imprimirColor espacio, 0d
-        imprimirColor espacio, 0d
-        imprimirColor espacio, 0d
+        imprimir individual, 15d
+        imprimir espacio, 0d
+        imprimir espacio, 0d
+        imprimir espacio, 0d
         inc si
         cmp si, 8d
         jnz ciclo
-    
-    ; Imprime cabeceras de filas
-    imprimirColor salto, 0d
+
+    imprimir salto, 0d
     xor si, si
-    ciclo2:
+    mov iteradorK, 0d
+    ciclo2: ; Ciclo que imprime las caberas de las filas
         xor di, di
         mov bl, cabecerasF[si]
         mov individual[0], bl
-        imprimirColor individual, 15d
-        imprimirColor espacio, 0d
+        imprimir individual, 15d
+        imprimir espacio, 0d
         mov iteradorI, 0d
 
-        ciclo3:
+        ciclo3: ;ciclo que imprime los valores de las celdas
             cmp si, 0
-            je filaPar
+            je pairFile
             cmp si, 1
-            je filaPar
+            je pairFile
             cmp si, 2
-            je filaPar
+            je pairFile
             cmp si, 3
-            je caracterVacio
+            je quitChars
             cmp si, 4
-            je caracterVacio
+            je quitChars
             cmp si, 5
-            je token
+            je blackToken
+            cmp si, 6
+            je blackToken
+            cmp si, 7
+            je blackToken
+            mov individual, " "
 
-            regreso:
-            imprimirColor individual, color
+            ; printRegister ah
+            regreso1:
+            imprimir individual, color
             inc di
             inc iteradorI
+
             cmp iteradorI, 8d
             jz reinicio
 
-            imprimirColor espacio, 0d
+            imprimir espacio, 0d
             mov bl, lineas[0]
-            mov individual, bl
-            imprimirColor individual, 15d
-            imprimirColor espacio, 0d
+            mov individual[0], bl
+            imprimir individual, 15d
+            imprimir espacio, 0d
             jmp ciclo3
 
-        caracterVacio:
+        quitChars:
             mov individual, " "
-            jmp regreso
+            jmp regreso1
 
-        token:
+        blackToken:
             xor ax, ax
             xor dx, dx
             mov ax, si
             mov bl, 2
             div bl
             cmp ah, 0
-            jne moverCeldaImpar
+            jne moveCellImpair
 
-            regresoImpar:
+            regresoImpair1:
             xor ax, ax
             xor dx, dx
             mov ax, di
             mov bl, 2
             div bl
             cmp ah, 0
-            je validarImpar
+            je validateImpair
             mov individual, " "
-            jmp regreso
+            jmp regreso1
 
-        moverCeldaImpar:
+        moveCellImpair:
             cmp di, 0
-            je incrementaImpar
-            jmp regresoImpar
+            je incrementForImpair
+            jmp regresoImpair1
 
-        validarImpar:
+        validateImpair:
             mov color, 14d
             mov individual[0], "B"
-            cmp si, 5
-            je llenarFila
-            cmp si, 7
-            je llenarFila
 
-        llenarFila:
+            cmp si, 5
+            je fillRow2
+            cmp si, 7
+            je fillRow2
+
+            mov pointer1, si
+            mov pointer2, di
+            obtenerIndice pointer1, pointer2
+            mov pointerGeneral, di
+            xor di, di
+            mov di, indice
+            mov tablero[di], 1d
+            xor di, di
+            mov di, pointerGeneral
+
+            jmp regreso1
+
+        fillRow2:
             mov pointer1, si
             dec di
             mov pointer2, di
-            
+            obtenerIndice pointer1, pointer2
+            inc di
+            mov pointerGeneral, di
+            xor di, di
+            mov di, indice
+            mov tablero[di], 1d
+            xor di, di
+            mov di, pointerGeneral
+            jmp regreso1
 
-        filaPar:
+        pairFile:
             xor ax, ax
             xor dx, dx
             mov ax, si
             mov bl, 2
             div bl
             cmp ah, 0
-            jne moverCelda
+            jne moveCell
 
-            regresoPar:
+            regresoPair1:
             xor ax, ax
             xor dx, dx
             mov ax, di
             mov bl, 2
             div bl
             cmp ah, 0
-            je validarPar
+            je validatePair
             mov individual, " "
-            jmp regreso
+            jmp regreso1
 
-        moverCelda:
+        moveCell:
             cmp di, 0
-            je incrementaImpar
-            jmp regresoPar
+            je incrementForImpair
+            jmp regresoPair1
 
-        incrementaImpar:
+        incrementForImpair:
             inc di
-            jmp regresoPar
+            jmp regresoPair1
 
-        reinicio:
+        validatePair:
+            ; inc di
+            mov color, 14d
+            mov individual[0], "N"
+
+            cmp si, 1
+            je fillRow
+
+            mov pointer1, si
+            mov pointer2, di
+            obtenerIndice pointer1, pointer2
+            mov pointerGeneral, di
+            xor di, di
+            mov di, indice
+            mov tablero[di], 2d
+            xor di, di
+            mov di, pointerGeneral
+            jmp regreso1
+
+        fillRow:
+            mov pointer1, si
+            dec di
+            mov pointer2, di
+            obtenerIndice pointer1, pointer2
+            inc di
+            mov pointerGeneral, di
+            xor di, di
+            mov di, indice
+            mov tablero[di], 2d
+            xor di, di
+            mov di, pointerGeneral
+            jmp regreso1
+
+        reinicio: ;ciclo que imprime la linea divisoria entre filas
+            ; printRegister ah
             cmp si, 8d
             jz reinicio2
+            mov iteradorI, 0d
+            imprimir salto, 0d
+            imprimir espacio, 0d
+            imprimir espacio, 0d
+            ciclo4:
+                mov bl, lineas[1]
+                mov individual[0], bl
+                imprimir individual, 15d
+                inc iteradorI
+                cmp iteradorI, 32d
+                jnz ciclo4
 
         reinicio2:
-            imprimirColor salto, 0d
+            imprimir salto, 0d
             inc si
             inc iteradorK
             cmp si, 8d
             jnz ciclo2
+    fin:
+endm
+
+obtenerIndice macro row, column
+    ;posicion[i][j] en el arreglo = i * numero columnas + j
+    mov ax, row
+    mov bx, 8d
+    mul bx
+    add ax, column
+    mov indice, ax
+endm
+
+Imprimir8bits macro registro
+    local cualquiera,noz
+    xor ax,ax
+    mov al,registro
+    mov cx,10
+    mov bx,3
+    cualquiera:
+    xor dx,dx
+    div cx
+    push dx
+    dec bx
+    jnz cualquiera
+    mov bx,3
+    noz:
+    pop dx
+    ImprimirNumero dl
+    dec bx
+    jnz noz
+endm
+
+Imprimir16bits macro registro
+    local cualquiera,noz
+    xor ax,ax
+    mov ax,registro
+    mov cx,10
+    mov bx,5
+    cualquiera:
+    xor dx,dx
+    div cx
+    push dx
+    dec bx
+    jnz cualquiera
+    mov bx,5
+    noz:
+    pop dx
+    ImprimirNumero dl
+    dec bx
+    jnz noz
+endm
+
+ImprimirNumero macro registro
+    push ax
+    push dx
+
+
+    mov dl,registro
+    ;ah = 2
+    add dl,48
+    mov ah,02h
+    int 21h
+
+
+    pop dx
+    pop ax
+endm
+
+ImprimirEspacio macro registro
+	push ax
+	push dx
+
+	mov registro, 13
+	CrearEspacio al
+	mov registro, 10
+	CrearEspacio al
+
+	pop dx
+	pop ax
+endm
+
+CrearEspacio macro registro
+	push ax
+	push dx
+
+	mov dl,registro
+	mov ah,02h
+	int 21h
+
+	pop dx
+	pop ax
+endm
+
+getIn macro
+    mov ah, 01h
+    int 21h
+endm
+
+leerHastaEnter macro entrada
+    local salto, fin
+
+    xor bx, bx ;Limpiando el registro
+    salto:
+        mov ah, 01h
+        int 21h
+        cmp al, 0dh ;Verificar si es un salto de linea lo que se esta leyendo
+        je fin
+        mov entrada[bx], al
+        inc bx
+        jmp salto
+
+    fin:
+        mov al, 24h ;Agregando un signo de dolar para eliminar el salto de linea
+        mov entrada[bx], al
 endm
--- Code 2/matriz.asm
+++ Code 2/matriz.asm
@@ -7,6 +7,9 @@ include macros.asm
 .data
     cabecerasF db "12345678$"
     cabecerasC db "ABCDEFGH$"
+    pregunta db "Escoge tu coordenada de destino$"
+    error db "Error de coordenadas de origen$"
+    ok db "Todo bien$"
     espacio db " $"
     individual db " $"
     lineas db "|-$"
@@ -16,6 +19,14 @@ include macros.asm
     iteradorK dw 0
     pointer1 dw 0
     pointer2 dw 0
+    indice dw 0
+    tablero db 64 dup(0), "$"
+    pointerGeneral dw 0
+    color db 0
+    bufferP1 db 50 dup("$"), "$"
+
+    fila dw 0
+    columna dw 0
 
 .code
     main proc
@@ -24,5 +35,66 @@ include macros.asm
 
         imprimirMatriz
 
+        ; mov fila, 5
+        ; mov columna, 1
+        ; mov bl, tablero[di]
+
+        turnoBlanco:
+        xor di, di
+        xor si, si
+        xor bx, bx
+
+        imprimir pregunta, 15d
+        ImprimirEspacio al
+        leerHastaEnter bufferP1
+
+        mov ah, bufferP1[0]
+        mov bh, bufferP1[2]
+
+        ciclo:
+            cmp cabecerasF[di], ah
+            je asignar
+            inc di
+            cmp di, 8d
+            jmp ciclo
+
+        asignar:
+            mov fila, di
+
+        xor di, di
+        ciclo2:
+            cmp cabecerasC[di], bh
+            je asignar2
+            inc di
+            cmp di, 8d
+            jmp ciclo2
+
+        asignar2:
+            mov columna, di
+
+        obtenerIndice fila, columna
+        mov si, indice
+
+        ; Imprimir16bits si
+
+        cmp tablero[si], 1d
+        je seguirJugando
+        jmp errorCasillas
+
+        seguirJugando:
+        imprimir ok, 15d
+        jmp fin
+
+        errorCasillas:
+        imprimir error, 15d
+
+        fin:
+
+        ; printRegister bl
+        ; ImprimirEspacio al
+        ; Imprimir16bits di
+
+        mov ax, 4C00H
+        INT 21H
     main endp
 end
